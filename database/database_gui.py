90a3ef550e1ba1d490463e9505d7dadaff29c462
meet447 <51074036+meet447@users.noreply.github.com>
2023-08-09 20:33:29 +0530

    Started working on api service!

--- database/database_gui.py
+++ database/database_gui.py
@@ -13,43 +13,54 @@ def main(page: ft.Page):
     page.scroll = True
     chats = load_chats()
     
+    def update_data():
+        chats = load_chats()
+
+        # Clear the existing controls
+        Column.controls.clear()
+
+        data_rows = []
+        for data in chats:
+            data_row = ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(data["name"])),
+                    ft.DataCell(ft.Text(data["id"])),
+                    ft.DataCell(ft.Text(data["short_desc"])),
+                    ft.DataCell(ft.Text(data["long_desc"])),
+                    ft.DataCell(ft.Image(data["image"])),
+                    ft.DataCell(ft.Text(data["origin"])),
+                    ft.DataCell(ft.Text(data["dialouge"])),
+                    ft.DataCell(ft.Text(data["chat"])),
+                ]
+            )
+            data_rows.append(data_row)
+
+        Column.controls.append(
+            ft.DataTable(
+                columns=[
+                    ft.DataColumn(ft.Text("Name")),
+                    ft.DataColumn(ft.Text("id")),
+                    ft.DataColumn(ft.Text("short_desc")),
+                    ft.DataColumn(ft.Text("long_desc")),
+                    ft.DataColumn(ft.Text("image")),
+                    ft.DataColumn(ft.Text("origin")),
+                    ft.DataColumn(ft.Text("dialouge")),
+                    ft.DataColumn(ft.Text("chat")),
+                ],
+                rows=data_rows,
+                column_spacing=40,
+                data_row_height=80,
+            )
+        )
+        page.update()
+
     Column = ft.Column(expand=True)
     page.add(Column)
-
-    data_rows = []
-    for data in chats:
-        data_row = ft.DataRow(
-            cells=[
-                ft.DataCell(ft.Text(data["name"])),
-                ft.DataCell(ft.Text(data["id"])),
-                ft.DataCell(ft.Text(data["short_desc"])),
-                ft.DataCell(ft.Text(data["long_desc"])),
-                ft.DataCell(ft.Image(data["image"])),
-                ft.DataCell(ft.Text(data["origin"])),
-                ft.DataCell(ft.Text(data["dialouge"])),
-                ft.DataCell(ft.Text(data["chat"])),
-            ]
-        )
-        data_rows.append(data_row)
-
-    Column.controls.append(
-        ft.DataTable(
-            columns=[
-                ft.DataColumn(ft.Text("Name")),
-                ft.DataColumn(ft.Text("id")),
-                ft.DataColumn(ft.Text("short_desc")),
-                ft.DataColumn(ft.Text("long_desc")),
-                ft.DataColumn(ft.Text("image")),
-                ft.DataColumn(ft.Text("origin")),
-                ft.DataColumn(ft.Text("dialouge")),
-                ft.DataColumn(ft.Text("chat")),
-            ],
-            rows=data_rows,
-            column_spacing=40,  # Adjust the spacing value as needed
-            data_row_height=80  ,
-        )
-    )
     
-    page.update()
+    update_button = ft.ElevatedButton(ft.Text("Update Data"), on_click=update_data)
+    Column.controls.append(update_button)
+
+    # Load the initial data
+    update_data()
 
 ft.app(target=main)
--- llm/rikadel.py
+++ llm/rikadel.py
@@ -1,31 +1,30 @@
 import requests
 
-url = "https://www.kapwing.com/api/v2/generate/images?"
+def rikadel(prompt):
 
-headers = {
-    "Accept": "application/json",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9,ja;q=0.8",
-    "Content-Type": "application/json",
-    "Cookie": "ssrSignedInCookie=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI1NTNhMjgzNDIxODAyM2MyNzcxYzQiLCJhY2NvdW50VHlwZSI6MCwibmFtZSI6Ik1hb3VTZW5wYWkiLCJlbWFpbCI6Im1hb3VzZW5wYWk0OEBnbWFpbC5jb20iLCJyb2xlIjoiZGVmYXVsdCIsImlhdCI6MTY4OTYwNTAyNiwiZXhwIjoxNzIxMTQxMDI2fQ.EoIHLlNcwfHg2gi3roeqr0eSRZZEH_2dfIzeuJQa-Gc",
-    "Referer": "https://www.kapwing.com/64b553a43d5edc00105170dd/studio/editor",
-    "Sec-Ch-Ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "\"Windows\"",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "X-Access-Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI1NTNhMjgzNDIxODAyM2MyNzcxYzQiLCJhY2NvdW50VHlwZSI6MCwibmFtZSI6Ik1hb3VTZW5wYWkiLCJlbWFpbCI6Im1hb3VzZW5wYWk0OEBnbWFpbC5jb20iLCJyb2xlIjoiZGVmYXVsdCIsImlhdCI6MTY4OTYwNTAyNiwiZXhwIjoxNzIxMTQxMDI2fQ.EoIHLlNcwfHg2gi3roeqr0eSRZZEH_2dfIzeuJQa-Gc"
-}
+    url = "https://www.kapwing.com/api/v2/generate/images?"
 
-parameter = { "prompt": "robot"}
+    headers = {
+        "Accept": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,ja;q=0.8",
+        "Content-Type": "application/json",
+        "Cookie": "ssrSignedInCookie=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI1NTNhMjgzNDIxODAyM2MyNzcxYzQiLCJhY2NvdW50VHlwZSI6MCwibmFtZSI6Ik1hb3VTZW5wYWkiLCJlbWFpbCI6Im1hb3VzZW5wYWk0OEBnbWFpbC5jb20iLCJyb2xlIjoiZGVmYXVsdCIsImlhdCI6MTY4OTYwNTAyNiwiZXhwIjoxNzIxMTQxMDI2fQ.EoIHLlNcwfHg2gi3roeqr0eSRZZEH_2dfIzeuJQa-Gc",
+        "Referer": "https://www.kapwing.com/64b553a43d5edc00105170dd/studio/editor",
+        "Sec-Ch-Ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "\"Windows\"",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "X-Access-Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI1NTNhMjgzNDIxODAyM2MyNzcxYzQiLCJhY2NvdW50VHlwZSI6MCwibmFtZSI6Ik1hb3VTZW5wYWkiLCJlbWFpbCI6Im1hb3VzZW5wYWk0OEBnbWFpbC5jb20iLCJyb2xlIjoiZGVmYXVsdCIsImlhdCI6MTY4OTYwNTAyNiwiZXhwIjoxNzIxMTQxMDI2fQ.EoIHLlNcwfHg2gi3roeqr0eSRZZEH_2dfIzeuJQa-Gc"
+    }
 
-response = requests.get(url, headers=headers, params=parameter)
-data = response.json()
+    parameter = { "prompt": prompt}
 
-# Extract URLs from the response
-urls = [item["url"] for item in data["data"]]
+    response = requests.get(url, headers=headers, params=parameter)
+    data = response.json()
 
-print(urls)    
+    return data
     
